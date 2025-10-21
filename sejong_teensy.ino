119783f7ff75e0c3c5cce1f61fe11d1bc71ac38b
noztosky <noztosky@gmail.com>
2025-10-21 12:28:13 +0900

    git 주소 추가

--- sejong_teensy.ino
+++ sejong_teensy.ino
@@ -4,7 +4,7 @@
 // mail:        custompilot1982@gmail.com
 // reference:   https://docs.google.com/spreadsheets/d/1F_w5camnlc7cn0YtFFpAKI0pPCTOrmvhNvpB1cN5wfA/edit?gid=0#gid=0
 // schematic:   https://pro.easyeda.com/editor#id=a5c96e1751ed435fa6f125d54dbc6e88,tab=13bb2ba02be84a75a610c58930748622@a5c96e1751ed435fa6f125d54dbc6e88|d9fc51fe403a4f2795ce8017a20d3c57@a5c96e1751ed435fa6f125d54dbc6e88|315d43a523ce4fe9989b48b5057603b1@a5c96e1751ed435fa6f125d54dbc6e88|*474c8ef620c74b1f851f36ad0037bf96@a5c96e1751ed435fa6f125d54dbc6e88|db6e5be36b69448e9c5321dd7cf94ad7@a5c96e1751ed435fa6f125d54dbc6e88
-// github:      
+// github:      https://github.com/noztosky/sejong_teensy41
 //
 
 #include <Arduino.h>
